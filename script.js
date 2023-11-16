26c70f2c4c41ad5170b2f4881d0e08d3ea4ecef8
abdulmusawwiralvi <contact.abdulmusawwir@gmail.com>
2023-11-16 18:19:43 +0600

    add all flowbite files

---
+++ script.js
@@ -0,0 +1,125 @@
+    function myFunction() {
+        var x = document.querySelector(".jsDropdown");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+    } 
+
+    function myFunctionTwo() {
+        var x = document.querySelector(".jsDropdownTwo");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+    } 
+
+    function myFunctionThree() {
+        var x = document.querySelector(".jsDropdownThree");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+    } 
+
+    function myFunctionFour() {
+        var x = document.querySelector(".jsDropdownFour");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+    } 
+
+    function myFunctionFive() {
+        var x = document.querySelector(".jsDropdownFive");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+    } 
+
+    function myFunctionSix() {
+        var x = document.querySelector(".jsDropdownSix");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+    } 
+
+    function myFunctionSeven() {
+        var x = document.querySelector(".jsDropdownSeven");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+    } 
+
+    function myFunctionEight() {
+        var x = document.querySelector(".jsDropdownEight");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+    } 
+
+    function myFunctionNine() {
+        var x = document.querySelector(".jsDropdownNine");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+    } 
+
+    function myFunctionTen() {
+        var x = document.querySelector(".jsDropdownTen");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+    } 
+
+    function myFunctionEleven() {
+        var x = document.querySelector(".jsDropdownEleven");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+    } 
+
+    function myFunctionTwelve() {
+        var x = document.querySelector(".jsDropdownTwelve");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+    } 
+
+    function myFunctionThirteen() {
+        var x = document.querySelector(".jsDropdownThirteen");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+    } 
+
+    function myFunctionFourteen() {
+        var x = document.querySelector(".jsDropdownFourteen");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+    } 
